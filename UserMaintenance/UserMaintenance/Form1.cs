145db4192798d4dae7b9d4a5929b3774ba68e2c2
Botond Boldizsár Karászi <botond.karaszi@stud.uni-corvinus.hu>
2021-09-30 17:28:57 +0200

    Fájlba írás

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace UserMaintenance
 
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,34 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+           
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                // Végigmegyünk a hallgató lista elemein
+                foreach (var s in users)
+                {
+                    
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+            }
+
         }
     }
 }
