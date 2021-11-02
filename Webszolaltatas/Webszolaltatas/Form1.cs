e10ea75e11d72701c11d3b88427933340e435e3f
Botond Boldizsár Karászi <botond.karaszi@stud.uni-corvinus.hu>
2021-11-02 20:06:38 +0100

    5. XML feldolgozása

--- Webszolaltatas/Webszolaltatas/Form1.cs
+++ Webszolaltatas/Webszolaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolaltatas.Entities;
 using Webszolaltatas.MnbServiceReference;
 
@@ -22,6 +23,15 @@ namespace Webszolaltatas
         {
             InitializeComponent();
 
+            Webservice();
+            XMLProcess();
+
+            dataGridView1.DataSource = Rates.ToList();
+
+        }
+
+        private void Webservice()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -29,14 +39,32 @@ namespace Webszolaltatas
                 currencyNames = "EUR",
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
-
             };
 
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+        }
 
-            dataGridView1.DataSource = Rates.ToList();
+        public void XMLProcess()
+        {
+            var xml = new XmlDocument();
+            xml.Load(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
