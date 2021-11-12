e7f0fb63510d915436e7fa33167a3abc179ce85a
Botond Boldizsár Karászi <botond.karaszi@stud.uni-corvinus.hu>
2021-11-12 17:20:39 +0100

    Gyár absztrakció kezelése

---
+++ Gyartosor/Gyartosor/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Gyartosor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyartosor.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
