959c2203882750e09b9be90f114a09fb27a22316
Botond Boldizsár Karászi <botond.karaszi@stud.uni-corvinus.hu>
2021-11-25 11:47:09 +0100

    Jelszó validálás teszt

--- UnitTestExample/UnitTestExample/Entities/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample/Entities/AccountControllerTestFixture.cs
@@ -30,5 +30,30 @@ namespace UnitTestExample.Entities
             Assert.AreEqual(expectedResult, actualResult);
 
         }
+
+
+
+
+
+        [Test,
+           TestCase("abcdAbcd", false),
+           TestCase("ABCD1234", false),
+           TestCase("abcd1234", false),
+           TestCase("Abcd123", false),
+           TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
     }
 }
